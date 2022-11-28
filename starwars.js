3103fbe73fba363e6bf999d88e664860cfd273e0
Tekllin <nathaniel.favario@gmail.com>
2022-11-28 16:03:59 +0100

    site nico

---
+++ starwars.js
@@ -0,0 +1,9 @@
+const animun = document.getElementById('animun');
+const animde = document.getElementById('animde');
+const body = document.getElementById('taille');
+
+function animation()
+{
+    animun.style.display = 'none';
+    animde.style.display = 'block';
+}
